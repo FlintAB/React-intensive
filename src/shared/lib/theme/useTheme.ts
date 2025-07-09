2b8143c43ca3dd67f34409c9abc4f1e38c95047f
Aleksey Baranovskiy <lari.bro@yandex.ru>
2025-07-09 18:50:35 +0300

    fix(useTheme)/ add context validation for hook

--- src/shared/lib/theme/useTheme.ts
+++ src/shared/lib/theme/useTheme.ts
@@ -4,5 +4,9 @@ import { ThemeContext } from "./ThemeContext"
 export const useTheme = () => {
    const context = useContext(ThemeContext);
 
+   if(!context) {
+      throw new Error('Контекст темы должен использоваться в компоненте, обёрнутом в ThemeProvider');
+   }
+
    return context;
 };
